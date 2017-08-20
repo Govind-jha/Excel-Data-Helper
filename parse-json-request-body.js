aabce149de67eb90c1532342c26a97124505102a
Govind-jha <govind10d@gmail.com>
2017-08-21 00:34:27 +0530

    v 1.1.0
    This is an application to parse json request body using data from excel sheet. We used json-templates package for json parsing and excel-data-helper module to read data from excel sheet.

---
+++ parse-json-request-body.js
@@ -0,0 +1,28 @@
+// get a reference to your required module
+var dataHelper = require('./excel-data-helper'),
+    parse = require("json-templates");
+
+// this is a sample json request template    
+var requestBodyTemplate = parse(
+    {   
+        acctNum  : '{{accountNumber}}',
+        acctType : '{{accountType}}'
+    }
+);      
+    
+// get raw json data in a local variable
+var data = dataHelper.testData;
+
+for(i = 1 ; i < dataHelper.numberOfTestCases(0); i++ ){
+    // get data array one row at a time by calling getTestData function
+    values = dataHelper.getTestData(0,i);
+ 
+    data_testCaseId = values[0];
+    data_accountNumber = values[1];
+    data_accountType   = values[2];
+
+    var requestBody = requestBodyTemplate({accountNumber : data_accountNumber, accountType : data_accountType});    
+    
+    console.log('Test Case : ' + data_testCaseId);
+    console.log(JSON.stringify(requestBody, null, 2));
+}
